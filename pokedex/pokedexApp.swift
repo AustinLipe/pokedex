c616f6d219150b14195adf93c8eb5da0e00a8710
AustinLipe <90396750+AustinLipe@users.noreply.github.com>
2021-10-19 08:30:21 -0500

    Initial Commit

---
+++ pokedex/pokedexApp.swift
@@ -0,0 +1,17 @@
+//
+//  pokedexApp.swift
+//  pokedex
+//
+//  Created by AMStudent on 10/19/21.
+//
+
+import SwiftUI
+
+@main
+struct pokedexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
